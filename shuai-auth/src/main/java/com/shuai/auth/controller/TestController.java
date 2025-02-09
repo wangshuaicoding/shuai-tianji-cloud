b5489b4263e60a96bfc00d27218606b21f7be0cb
少帅 <wangshuaicoding@163.com>
2025-02-09 15:26:18 +0800

    test1代码

---
+++ shuai-auth/src/main/java/com/shuai/auth/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.shuai.auth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("hello world=====1");
+    }
+}
